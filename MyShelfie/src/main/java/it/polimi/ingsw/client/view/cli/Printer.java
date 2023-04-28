b9a4248fed3b527d4269e56dd6b298880a5f341e
Luca Guffanti <luca.guffanti.000@gmail.com>
2023-04-29 00:22:15 +0200

    CLI graphic update: added board, table, personal goal card printing (not finished CSVs)

--- MyShelfie/src/main/java/it/polimi/ingsw/client/view/cli/Printer.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/client/view/cli/Printer.java
@@ -1,5 +1,16 @@
 package it.polimi.ingsw.client.view.cli;
 
+import it.polimi.ingsw.server.controller.utils.GameObjectConverter;
+import it.polimi.ingsw.server.model.Board;
+import it.polimi.ingsw.server.model.Sack;
+import it.polimi.ingsw.server.model.cards.ObjectTypeEnum;
+import it.polimi.ingsw.server.model.utils.CsvToBoardParser;
+import it.polimi.ingsw.server.model.utils.CsvToShelfParser;
+import jdk.jfr.Label;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This class implements methods and contains attributes useful for
  * printing game objects and messages.
@@ -78,6 +89,43 @@ public class Printer {
     public static final String PURPLE_BACKGROUND_BRIGHT = "\033[0;105m";
     public static final String CYAN_BACKGROUND_BRIGHT = "\033[0;106m";
     public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m";
+    public static final String LIGHT_BROWN_BACKGROUND_BRIGHT = "\033[48;5;137m";
+    public static final String DARK_BROWN_BACKGROUND_BRIGHT = "\033[48;5;94m";
+
+
+
+    public static final Map<ObjectTypeEnum, String> objectTypeToRender = new HashMap<>();
+    public static final Map<String, ObjectTypeEnum[][]> personalIdToRender = new HashMap<>();
+    public static final Map<String, String> commonIdToRender = new HashMap<>();
+
+
+
+    static {
+        objectTypeToRender.put(ObjectTypeEnum.CAT, GREEN_BACKGROUND_BRIGHT+" "+RESET);
+        objectTypeToRender.put(ObjectTypeEnum.TROPHY, CYAN_BACKGROUND_BRIGHT+" "+RESET);
+        objectTypeToRender.put(ObjectTypeEnum.TOY, YELLOW_BACKGROUND_BRIGHT+" "+RESET);
+        objectTypeToRender.put(ObjectTypeEnum.BOOK, WHITE_BACKGROUND_BRIGHT+" "+RESET);
+        objectTypeToRender.put(ObjectTypeEnum.FRAME, BLUE_BACKGROUND_BRIGHT+" "+RESET);
+        objectTypeToRender.put(ObjectTypeEnum.PLANT, PURPLE_BACKGROUND_BRIGHT+" "+RESET);
+
+        try {
+            personalIdToRender.put("1", CsvToShelfParser.simpleConvert("src/main/assets/cards/personalGoalCSV/1.csv"));
+            personalIdToRender.put("2", CsvToShelfParser.simpleConvert("src/main/assets/cards/personalGoalCSV/2.csv"));
+            personalIdToRender.put("3", CsvToShelfParser.simpleConvert("src/main/assets/cards/personalGoalCSV/3.csv"));
+            personalIdToRender.put("4", CsvToShelfParser.simpleConvert("src/main/assets/cards/personalGoalCSV/4.csv"));
+            personalIdToRender.put("5", CsvToShelfParser.simpleConvert("src/main/assets/cards/personalGoalCSV/5.csv"));
+            personalIdToRender.put("6", CsvToShelfParser.simpleConvert("src/main/assets/cards/personalGoalCSV/6.csv"));
+            personalIdToRender.put("7", CsvToShelfParser.simpleConvert("src/main/assets/cards/personalGoalCSV/7.csv"));
+            personalIdToRender.put("8", CsvToShelfParser.simpleConvert("src/main/assets/cards/personalGoalCSV/8.csv"));
+            personalIdToRender.put("9", CsvToShelfParser.simpleConvert("src/main/assets/cards/personalGoalCSV/9.csv"));
+            personalIdToRender.put("10", CsvToShelfParser.simpleConvert("src/main/assets/cards/personalGoalCSV/10.csv"));
+            personalIdToRender.put("11", CsvToShelfParser.simpleConvert("src/main/assets/cards/personalGoalCSV/11.csv"));
+            personalIdToRender.put("12", CsvToShelfParser.simpleConvert("src/main/assets/cards/personalGoalCSV/12.csv"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
     public static void log(String s) {
         System.out.println(s);
@@ -85,6 +133,109 @@ public class Printer {
 
     public static void error(String s) {
         System.out.println(RED_BOLD+s+RESET);
+    }
+
+    public static void printShelf(ObjectTypeEnum[][] shelf) {
+        int wantedWidth = 3;
+        for (int x = 0; x < shelf[0].length; x++) {
+            System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+"     "+RESET);
+        }
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+" "+RESET);
+        System.out.print("\n");
+        for (int y = 0; y < shelf.length; y++) {
+            System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+BLACK_BOLD+" "+y+" "+RESET);
+
+            for (int x = 0; x < shelf[0].length; x++) {
+                System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+" "+RESET);
+                for (int rep = 0; rep < wantedWidth; rep++) {
+                    if (shelf[y][x] == null) {
+                        System.out.print(DARK_BROWN_BACKGROUND_BRIGHT+" "+RESET);
+                    } else {
+                        System.out.print(objectTypeToRender.get(shelf[y][x]));
+                    }
+                }
+            }
+            System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+"   "+RESET);
+            System.out.print("\n");
+        }
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+"   "+RESET);
+        for (int x = 0; x < shelf[0].length; x++) {
+            System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+BLACK_BOLD+"  "+x+" "+RESET);
+        }
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+"   "+RESET);
+        System.out.print("\n");
+    }
+
+    public static void printBoard(ObjectTypeEnum[][] board) {
+        int wantedWidth = 3;
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+"   "+RESET);
+        for (int x = 0; x < board[0].length; x++) {
+            System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+BLACK_BOLD+"  "+x+" "+RESET);
+        }
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+"   "+RESET);
+        System.out.print("\n");
+        for (int y = 0; y < board.length; y++) {
+            System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+BLACK_BOLD+" "+y+" "+RESET);
+
+            for (int x = 0; x < board[0].length; x++) {
+                System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+" "+RESET);
+                for (int rep = 0; rep < wantedWidth; rep++) {
+                    if (board[y][x] == null) {
+                        System.out.print(DARK_BROWN_BACKGROUND_BRIGHT+" "+RESET);
+                    } else {
+                        System.out.print(objectTypeToRender.get(board[y][x]));
+                    }
+                }
+            }
+            System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+BLACK_BOLD+" "+y+" "+RESET);
+            System.out.print("\n");
+        }
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+"   "+RESET);
+        for (int x = 0; x < board[0].length; x++) {
+            System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+BLACK_BOLD+"  "+x+" "+RESET);
+        }
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+"   "+RESET);
+        System.out.print("\n");
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+BLACK_BOLD+"COORDINATE SYSTEM:       "+RESET+"\n");
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+BLACK_BOLD+"y      With y increasing "+RESET+"\n");
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+BLACK_BOLD+"║      going through the "+RESET+"\n");
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+BLACK_BOLD+"╚══x   rows              "+RESET+"\n");
+    }
+    public static void printCommonGoalCard(String id) {
+
+    }
+
+    public static void printPersonalGoalCard(String id) {
+        printShelf(personalIdToRender.get(id));
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+WHITE_BOLD_BRIGHT+" x 1 "+RESET);
+        for (int x = 2; x < 7; x++) {
+            System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+WHITE_BOLD_BRIGHT+"  "+x+" "+RESET);
+        }
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+WHITE_BOLD_BRIGHT+" "+RESET);
+        System.out.print("\n");
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+WHITE_BOLD_BRIGHT+" * 1 "+RESET);
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+WHITE_BOLD_BRIGHT+"  2 "+RESET);
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+WHITE_BOLD_BRIGHT+"  4 "+RESET);
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+WHITE_BOLD_BRIGHT+"  6 "+RESET);
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+WHITE_BOLD_BRIGHT+"  9 "+RESET);
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+WHITE_BOLD_BRIGHT+" 12 "+RESET);
+        System.out.print(LIGHT_BROWN_BACKGROUND_BRIGHT+WHITE_BOLD_BRIGHT+" "+RESET);
+        System.out.print("\n");
+    }
+
+    @Label("DEBUG")
+    public static void main(String[] args) throws Exception {
+        String s = "src/test/resources/shelfTEST/endGameShelfCheck_full.csv";
+        Printer.printShelf(GameObjectConverter.simplifyShelfIntoMatrix(CsvToShelfParser.convert(s)));
+        System.out.print("\n");
+        s = "src/test/resources/shelfTEST/diagonalValid.csv";
+        Printer.printShelf(GameObjectConverter.simplifyShelfIntoMatrix(CsvToShelfParser.convert(s)));
+        s = "src/test/resources/boardTEST/expectedCellObjectCard_4players.csv";
+
+        Sack sack = new Sack();
+        Board b = new Board(4, sack);
+        Printer.printBoard(GameObjectConverter.simplifyBoardIntoMatrix(b));
+        printPersonalGoalCard("1");
     }
 
 }
--- MyShelfie/src/main/java/it/polimi/ingsw/server/model/utils/CsvToShelfParser.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/server/model/utils/CsvToShelfParser.java
@@ -26,6 +26,15 @@ public class CsvToShelfParser {
             "X", Optional.empty()
             );
 
+    private static final Map<String, ObjectTypeEnum> strToCellObjectCardMap = Map.of(
+            "G", ObjectTypeEnum.CAT,
+            "B", ObjectTypeEnum.FRAME,
+            "W", ObjectTypeEnum.BOOK,
+            "Y", ObjectTypeEnum.TOY,
+            "C", ObjectTypeEnum.TROPHY,
+            "M", ObjectTypeEnum.PLANT
+    );
+
 
 
     public static Shelf convert(String path) throws Exception {
@@ -54,5 +63,35 @@ public class CsvToShelfParser {
         Shelf shelf = new Shelf();
         shelf.setCells(cells);
         return shelf;
+    }
+
+    public static ObjectTypeEnum[][] simpleConvert(String path) {
+        ObjectTypeEnum[][] cells = new ObjectTypeEnum[Constants.SHELF_HEIGHT][Constants.SHELF_LENGTH];
+
+        try(CSVReader reader = new CSVReader(new FileReader(path)))
+        {
+            String [] nextLine;
+            int x = 0, y = 0;
+
+            // Reads one line at a time from the csv file (nextLine is an array of strings)
+            while ((nextLine = reader.readNext()) != null)
+            {
+                for (String word: nextLine) {
+                    if (word.equals("X")) {
+                        cells[y][x] = null;
+                    } else {
+                        cells[y][x] = strToCellObjectCardMap.get(word);
+                    }
+                    x++;
+                }
+                y++;
+                x = 0;
+            }
+        }
+        catch (IOException | CsvValidationException e) {
+            e.printStackTrace();
+        }
+
+        return cells;
     }
 }
