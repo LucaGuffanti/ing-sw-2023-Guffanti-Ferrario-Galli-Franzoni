106e5023e8cc8003cb99adb5c46176c83c8136c1
Marco Galli <marco01gll@gmail.com>
2023-05-02 14:13:32 +0200

    First implementation of a basic gui

--- MyShelfie/src/main/java/it/polimi/ingsw/client/view/cli/Cli.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/client/view/cli/Cli.java
@@ -14,7 +14,7 @@ import java.beans.PropertyChangeListener;
 import java.io.InputStream;
 import java.util.Map;
 
-public class Cli extends UserInterface implements PropertyChangeListener {
+public class Cli implements UserInterface, PropertyChangeListener {
 
 
     private StateContainer stateContainer;
