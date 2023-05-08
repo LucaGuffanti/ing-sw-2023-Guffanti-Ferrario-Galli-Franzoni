049d3a94d79c8eb3a704b0fd4c798695da15fd53
Marco Galli <marco01gll@gmail.com>
2023-05-08 17:33:45 +0200

    Implemented possibility to set ip address of server

--- MyShelfie/src/main/java/it/polimi/ingsw/server/ServerStart.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/server/ServerStart.java
@@ -3,6 +3,8 @@ package it.polimi.ingsw.server;
 import it.polimi.ingsw.network.ServerNetworkHandler;
 import it.polimi.ingsw.network.utils.ServerConfigurationData;
 
+import java.util.Scanner;
+
 /**
  * <h1>ServerStart</h1>
  * The main entry point of the server. Network interface information is automatically retrieved
@@ -16,7 +18,18 @@ public class ServerStart {
     public static final String serverConfigPath = "src/config/serverConfig.json";
     public static void main(String[] args) {
 
+        System.out.println("Insert your LAN IP address if you had any problem before (due " +
+                "to java not recognizing your network interface). Simply press enter if you don't want" +
+                "to insert any address.");
+        System.out.print("IP address: ");
+        Scanner scanner = new Scanner(System.in);
+        String ipAddress = scanner.nextLine();
+
         ServerConfigurationData data = new ServerConfigurationData();
+        if (ipAddress.trim().length()>0) {
+            System.setProperty("java.rmi.server.hostname", ipAddress);
+            data.setIpAddress(ipAddress);
+        }
         ServerNetworkHandler serverNetworkHandler = new ServerNetworkHandler(data);
         serverNetworkHandler.activateHandler();
 
